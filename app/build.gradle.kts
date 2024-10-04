af3a0cd07ab0d865be3cfab6ada5b1bf8303a20d
vnzoliveira <tploxvrdct@protonmail.ch>
2024-10-03 23:19:05 -0300

    adicionando dependencia appcompact para toolbars

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
